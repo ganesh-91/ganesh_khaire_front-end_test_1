83e7dfd19d2a69445c11cababf38ab16f2dc44a2
ganesh.khaire@knorex.com <ganesh.khaire@knorex.com>
2019-08-31 13:00:19 +0530

    commit for rest funtoins

--- problem one/index.js
+++ problem one/index.js
@@ -1,13 +1,31 @@
 
 var arr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'z', 'y']
 
-var computeSum = function (char) {
-    console.log()
-    // return { computeSum }
-    var charIndex = arr.indexOf(char);
+
+// function for getting number for char
+const computeSum = function (char) {
+    const charIndex = arr.indexOf(char);
     if (char === 'a') {
         return 1;
     } else {
         return (computeSum(arr[charIndex - 1]) * 2 + arr.indexOf(char) + 1);
     }
 }
+
+// function for getting sum from string
+const parseString = function (string) {
+    string.split('').map((char) => {
+        console.log(computeSum(char));
+    });
+}
+
+// function for getting char from number
+const findChar = function (number) {
+    arr.forEach((elm) => {
+        if (computeSum(elm) === number) {
+            console.log(elm);
+        }
+    })
+}
+
+
